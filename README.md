cdd7bc2d11efd7032e2e758303213492249cf78d
Justin Bobrick <j.bobrick@outlook.com>
2023-07-12 22:54:37 -0700

    fixed timer and next question

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # coding-quiz
 Creating a multiple choice coding quiz
 
+These are the sites I referenced when creating this quiz:
+https://www.w3schools.com/js/js_arrays.asp#:~:text=Creating%20an%20Array,arrays%20with%20the%20const%20keyword.
+https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS (used this for questions)
--- assets/script.js
+++ assets/script.js
@@ -10,74 +10,7 @@ var timer = document.getElementById("timerr");
 var nextbutton = document.getElementById("next-button");
 
 var currentQuestion, randomQuestion;
-
-/* Making the start button clickable to begin the quiz */
-startbutton.addEventListener("click", startQuiz);
-answerEl.addEventListener("click", nextQuestion);
-
-/* Starting the game and showing the first question */
-function startQuiz() {
-  console.log("You've been clicked");
-  startbutton.classList.add("hide");
-  beginGame.classList.remove("hide");
-  randomQuestion = questions.sort(() => Math.random() * questions.length);
-  currentQuestion = 0;
-  nextQuestion();
-}
-
-/* Timer ticking down */
-function countdown() {
-  var timeLeft = 60;
-  var timeInterval = setInterval(function () {
-    if (timeLeft > 1) {
-      timer.textContent = timeLeft + " seconds.";
-      timeLeft--;
-    } else {
-      timer.textContent = "";
-      clearInterval(timeInterval);
-    }
-  }, 1000);
-}
-
-/* Adding an event listener and showing a random next question */
-answerEl.addEventListener("click", nextQuestion);
-
-function nextQuestion() {
-  resetQuestion();
-  showQuestion(randomQuestion[currentQuestion]);
-}
-
-/* Showing the question and displaying or choosing each answer */
-function showQuestion(question) {
-  questionEl.innerText = question.question;
-  question.answers.forEach((answer) => {
-    var buttons = document.createElement("button");
-    buttons.innerText = answer.text;
-    buttons.classList.add("btn");
-    if (answer.correct) {
-      buttons.dataset.correct = answer.correct;
-    }
-    buttons.addEventListener("click", selectAnswer);
-    answerEl.appendChild(buttons);
-  });
-}
-
-/* Function for selecting an answer */
-function selectAnswer(select) {
-  var selection = select.target;
-  var correct = select.dataset.correct;
-  setStatusClass(document.body, correct);
-}
-
-/* Removing answer questions to replace them with answers from the array */
-function resetQuestion() {
-  answerbutton.classList.add("hide");
-  answerbutton2.classList.add("hide");
-  answerbutton3.classList.add("hide");
-  answerbutton4.classList.add("hide");
-}
-
-/* Setting an array of questions and answers to randomly select from */
+/* Setting an array of questions and answers to select from */
 var questions = [
   {
     question: "Inside which HTML element do we put JavaScript?",
@@ -87,6 +20,8 @@ var questions = [
       { text: "Java Tag", correct: false },
       { text: "Section Tag", correct: false },
     ],
+  },
+  {
     question: "Where is the correct place to insert a JavaScript?",
     answers: [
       { text: "Body", correct: false },
@@ -95,4 +30,98 @@ var questions = [
       { text: "None of the above", correct: false },
     ],
   },
+  {
+    question: "The external JavaScript file must contain the <script> tag.",
+    answers: [
+      { text: "True", correct: false },
+      { text: "False", correct: true },
+    ],
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    answers: [
+        { text: "function = myFunction()", correct: false },
+        { text: "function myfunction()", correct: true },
+        { text: "function:myFunction", correct: false },
+    ]
+  },
 ];
+
+/* Making the start button clickable to begin the quiz */
+startbutton.addEventListener("click", startQuiz);
+answerEl.addEventListener("click", checkingAnswer);
+
+/* Starting the game and showing the first question */
+function startQuiz() {
+  console.log("You've been clicked");
+  startbutton.classList.add("hide");
+  beginGame.classList.remove("hide");
+  //   randomQuestion = questions.sort(() => Math.random() * questions.length);
+  countdown();
+  currentQuestion = 0;
+  nextQuestion();
+}
+
+/* Timer ticking down */
+function countdown() {
+  var timeLeft = 60;
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 0) {
+      timer.textContent = timeLeft + " seconds left";
+      timeLeft--;
+    } else {
+      timer.textContent = "";
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+
+/* Adding an event listener and showing next question */
+function checkingAnswer(event) {
+  console.log(event.target);
+  currentQuestion++;
+  nextQuestion();
+}
+
+function nextQuestion() {
+  console.log(questions);
+  console.log(currentQuestion);
+  showQuestion(questions[currentQuestion]);
+}
+
+/* Showing the question and displaying or choosing each answer */
+function showQuestion(question) {
+  questionEl.innerText = question.question;
+  answerEl.innerHTML=""
+  question.answers.forEach((answer) => {
+    var buttons = document.createElement("button");
+    buttons.innerText = answer.text;
+    buttons.classList.add("btn");
+    if (answer.correct) {
+      buttons.dataset.correct = answer.correct;
+    }
+    /*buttons.addEventListener("click", selectAnswer);*/
+    answerEl.appendChild(buttons);
+  });
+}
+
+/* Function for selecting an answer */
+// function selectAnswer(select) {
+//   var selection = select.target;
+//   var correct = select.dataset.correct;
+//   setStatusClass(document.body, correct);
+// }
+
+/* Removing answer questions to replace them with answers from the array */
+// function resetQuestion() {
+//   answerbutton.textContent = "";
+//   answerbutton2.textContent = "";
+//   answerbutton3.textContent = "";
+//   answerbutton4.textContent = "";
+//   answerbutton.classList.add("hide");
+//   answerbutton2.classList.add("hide");
+//   answerbutton3.classList.add("hide");
+//   answerbutton4.classList.add("hide");
+// }
+
+/* Setting an array of questions and answers to randomly select from */
